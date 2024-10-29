4f9a6785530077db6f5b5629a0172913e80b803c
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2024-10-29 14:33:09 +0100

    create end point, implement post data sending

--- index.py
+++ index.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+
+@app.route("/solve", methods=['POST'])
+def calculate():
+    print("Form Data:", request.form)
+    print("JSON Data:", request.json)
+
+
+    return jsonify({"status": "ok"})
